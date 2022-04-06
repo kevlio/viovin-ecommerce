b88b48afc47c8b0e05dbca1e4c4b0b1f1397a37b
Sandy <sandy.congreve@cmeducations.se>
2022-04-06 11:48:06 +0200

    Added images

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,112 +1,91 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
+import { ImgContainer } from "../Slider/Slider.styles";
+
 
 const Container = styled.div`
-height: 100%;
-padding: 10% 5% 10% 5%;
-background-color: transparent;
-
-
-
-
+  height: 100%;
+  padding: 10% 5% 10% 5%;
+  display: flex;
+  justify-content: center;
+  background-image: url("https://github.com/congreves/viovin-ecommerce/blob/main/src/assets/Benezet%20(22).jpg");
+  
 `;
-const Wrapper = styled.div`
-display:flex;
-justify-content: center;
-
-`
-;
-
 const ImageContainer = styled.img`
-height: 550px;
-width: 550px;
-border-radius: 50%;
-opacity: 0.9;
-
 
 `
-;
 
 const Form = styled.form`
-width: 30%;
-padding: 3em 2em 3em 2em;
-border-radius: 1em;
-background: rgba( 255, 255, 255, 0.25 );
-box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.01 );
-backdrop-filter: blur( 4px );
--webkit-backdrop-filter: blur( 4px );
-border-radius: 10px;
-border: 1px solid rgba( 255, 255, 255, 0.18 );
-
-
-
-
-
+  width: 30%;
+  padding: 3em 2em 3em 2em;
+  border-radius: 1em;
+  background: rgba(255, 255, 255, 0.25);
+ 
 `;
 const Title = styled.h2`
-display: block;
-padding: 1em 1em;
-font-weight: 200;
-
-`
+  display: block;
+  padding: 1em 1em;
+  font-weight: 200;
+`;
 const ContactInfo = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: flex-start;
-align-items: left;
-padding: 1em 1em;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: left;
+  padding: 1em 1em;
+`;
 const Label = styled.label`
-flex: 1;
-padding: 1em 1em;
-
+  flex: 1;
+  padding: 1em 1em;
 `;
 
-const Input = styled.input `
-border-style: none;
-width: 100%;
-padding: 1em 1em;
-height: 100%;
-`
+const Input = styled.input`
+  border-style: none;
+  width: 100%;
+  padding: 1em 1em;
+  height: 100%;
+`;
 const Message = styled.div`
-display: flex;
-flex-direction: column;
-flex: 1;
-padding: 1em 1em;
-`
-
-
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 1em 1em;
+`;
 
 function ContactForm() {
   return (
     <div>
-        
-        <Container>
-            <Wrapper>
-<ImageContainer src="https://i.ibb.co/gZYX7Gb/jonathan-farber-Tht21-XTk-QE-unsplash.jpg">
-
-</ImageContainer>
-            
+      <Container>
+        <ImgContainer src="https://github.com/congreves/viovin-ecommerce/blob/main/src/assets/Benezet%20(22).jpg"></ImgContainer>
         <Form>
-            <Title>KONTAKTA OSS</Title>
-            <ContactInfo>
-            <Label >Namn</Label>
-            <Input placeholder='Skriv ditt namn..' name="förnamn" type='text'></Input>
+          <Title>KONTAKTA OSS</Title>
+          <ContactInfo>
+            <Label>Namn</Label>
+            <Input
+              placeholder="Skriv ditt namn.."
+              name="förnamn"
+              type="text"
+            ></Input>
             <Label>Email</Label>
-            <Input placeholder='Skriv in email' email="emailadress" type='text'></Input>
+            <Input
+              placeholder="Skriv in email"
+              email="emailadress"
+              type="text"
+            ></Input>
             <Label>Företag /Privatperson</Label>
-            <Input placeholder='Skriv in företag/privatperson' type='text'></Input>
-            </ContactInfo>
-            <Message>
-            <Label>Meeddelande</Label>
-            <Input placeholder='Lämna ett meddelande här..' type='text'></Input>
-            </Message>
+            <Input
+              placeholder="Skriv in företag/privatperson"
+              type="text"
+            ></Input>
+          </ContactInfo>
+          <Message>
+            <Label>Meddelande</Label>
+            <Input placeholder="Lämna ett meddelande här.." type="text"></Input>
+          </Message>
         </Form>
-        </Wrapper>
-        </Container>
-
+      </Container>
     </div>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
--- src/data.js
+++ src/data.js
@@ -121,3 +121,44 @@ export const sliderItems = [
 <a href="https://ibb.co/QHS7zpy"><img src="https://i.ibb.co/pKV6GbY/Screenshot-2022-02-23-at-15-44-53-removebg-preview.png" alt="Screenshot-2022-02-23-at-15-44-53-removebg-preview" border="0"></a>
 <a href="https://imgbb.com/"><img src="https://i.ibb.co/hmdSSLm/Dore-Baguette-Couronne-Ico-ne-de-Cercle-Clerge-Logo-2-removebg-preview-2.png" alt="Dore-Baguette-Couronne-Ico-ne-de-Cercle-Clerge-Logo-2-removebg-preview-2" border="0"></a>
 <a href="https://imgbb.com/"><img src="https://i.ibb.co/Njx00gj/Dore-Baguette-Couronne-Ico-ne-de-Cercle-Clerge-Logo-2-removebg-preview-1.png" alt="Dore-Baguette-Couronne-Ico-ne-de-Cercle-Clerge-Logo-2-removebg-preview-1" border="0"></a>*/
+
+
+
+
+
+
+/* <a href="https://ibb.co/dgtRCM7"><img src="https://i.ibb.co/Wt0L85s/003.jpg" alt="003" border="0"></a>
+<a href="https://ibb.co/frPVtFZ"><img src="https://i.ibb.co/NWHgpCf/011.jpg" alt="011" border="0"></a>
+<a href="https://ibb.co/cgG2PFg"><img src="https://i.ibb.co/0DgYkMD/017.jpg" alt="017" border="0"></a>
+<a href="https://ibb.co/3mbs8Yw"><img src="https://i.ibb.co/B2JP7Km/027.jpg" alt="027" border="0"></a>
+<a href="https://ibb.co/wKTvdhs"><img src="https://i.ibb.co/CnqrwK2/036.jpg" alt="036" border="0"></a>
+<a href="https://ibb.co/pJZ6QM8"><img src="https://i.ibb.co/dG6stVq/051.jpg" alt="051" border="0"></a>
+<a href="https://ibb.co/KsfRhM3"><img src="https://i.ibb.co/SmMWyh9/067.jpg" alt="067" border="0"></a>
+<a href="https://ibb.co/GWQJ5J0"><img src="https://i.ibb.co/bs53K3P/Bellefontaine-4.jpg" alt="Bellefontaine-4" border="0"></a>
+<a href="https://ibb.co/Y49gnFp"><img src="https://i.ibb.co/GfwmY4c/Bellefontaine-6.jpg" alt="Bellefontaine-6" border="0"></a>
+<a href="https://ibb.co/XpbTyXT"><img src="https://i.ibb.co/wCR1Jy1/Bellefontaine-11.jpg" alt="Bellefontaine-11" border="0"></a>
+<a href="https://ibb.co/VjzbryL"><img src="https://i.ibb.co/Gx8GBwd/Bellefontaine-13.jpg" alt="Bellefontaine-13" border="0"></a>
+<a href="https://ibb.co/v3DtLPS"><img src="https://i.ibb.co/k4Mdq8t/Bellefontaine-16.jpg" alt="Bellefontaine-16" border="0"></a>
+<a href="https://ibb.co/RCGLZjZ"><img src="https://i.ibb.co/mHrQ1N1/Bellefontaine-20.jpg" alt="Bellefontaine-20" border="0"></a>
+<a href="https://ibb.co/d2bMQpN"><img src="https://i.ibb.co/WyD5czr/Bellefontaine-37.jpg" alt="Bellefontaine-37" border="0"></a>
+<a href="https://ibb.co/NZ7cVVr"><img src="https://i.ibb.co/Rz7RvvN/Bellefontaine-60.jpg" alt="Bellefontaine-60" border="0"></a>
+<a href="https://ibb.co/TqLmMVT"><img src="https://i.ibb.co/n8CzMH0/Benezet-2.jpg" alt="Benezet-2" border="0"></a>
+<a href="https://ibb.co/w6cSv14"><img src="https://i.ibb.co/Yj0crGh/Benezet-10.jpg" alt="Benezet-10" border="0"></a>
+<a href="https://ibb.co/NTRLXfX"><img src="https://i.ibb.co/sW7Fhch/Benezet-19.jpg" alt="Benezet-19" border="0"></a>
+<a href="https://ibb.co/mD8QHFm"><img src="https://i.ibb.co/dWgnQJS/Benezet-22.jpg" alt="Benezet-22" border="0"></a>
+<a href="https://ibb.co/YNVZsWc"><img src="https://i.ibb.co/K58wvG2/Benezet-42.jpg" alt="Benezet-42" border="0"></a>
+<a href="https://ibb.co/2YZkCk9"><img src="https://i.ibb.co/cXYrZrB/Benezet-58.jpg" alt="Benezet-58" border="0"></a>
+<a href="https://ibb.co/bRP5Z5W"><img src="https://i.ibb.co/qFdYfYy/Benezet-84.jpg" alt="Benezet-84" border="0"></a>
+<a href="https://ibb.co/r5TQ6Wx"><img src="https://i.ibb.co/xzP2j41/Cha-teau-Bellefontaine-CC-11.jpg" alt="Cha-teau-Bellefontaine-CC-11" border="0"></a>
+<a href="https://ibb.co/HxrwZg0"><img src="https://i.ibb.co/4YjvkNc/DSC-0276.jpg" alt="DSC-0276" border="0"></a>
+<a href="https://ibb.co/CPkfbFW"><img src="https://i.ibb.co/kmRt4FM/IMG-7387.jpg" alt="IMG-7387" border="0"></a>
+<a href="https://ibb.co/cFQKBYW"><img src="https://i.ibb.co/Lt1DFNT/Photo-BF1.jpg" alt="Photo-BF1" border="0"></a>
+<a href="https://ibb.co/NjqkG5b"><img src="https://i.ibb.co/gT8XcNh/Photo-BF2.jpg" alt="Photo-BF2" border="0"></a>
+<a href="https://ibb.co/zPC1zPj"><img src="https://i.ibb.co/CV4qfVN/Photo-salle-mariage.jpg" alt="Photo-salle-mariage" border="0"></a>
+<a href="https://ibb.co/vZGJ6WJ"><img src="https://i.ibb.co/Bqxg5Jg/Photos-Saint-benezet-27.jpg" alt="Photos-Saint-benezet-27" border="0"></a>
+<a href="https://ibb.co/gMdPqwp"><img src="https://i.ibb.co/LzrJj6W/Photos-Saint-benezet-37.jpg" alt="Photos-Saint-benezet-37" border="0"></a>
+<a href="https://ibb.co/zmGSY2y"><img src="https://i.ibb.co/WHgcwzb/Photos-Saint-benezet-51.jpg" alt="Photos-Saint-benezet-51" border="0"></a>
+<a href="https://ibb.co/WK45NsY"><img src="https://i.ibb.co/s3dRptM/Photos-Saint-benezet-137.jpg" alt="Photos-Saint-benezet-137" border="0"></a>
+<a href="https://ibb.co/NrjX5ww"><img src="https://i.ibb.co/cwFP0zz/Photos-Saint-benezet-142.jpg" alt="Photos-Saint-benezet-142" border="0"></a>
+<a href="https://ibb.co/gmB7ggg"><img src="https://i.ibb.co/BcSPsss/Photos-Saint-benezet-147.jpg" alt="Photos-Saint-benezet-147" border="0"></a>
+<a href="https://ibb.co/kXpZZtn"><img src="https://i.ibb.co/Fz9ppFv/ST-BENEZET.jpg" alt="ST-BENEZET" border="0"></a>*/ 
